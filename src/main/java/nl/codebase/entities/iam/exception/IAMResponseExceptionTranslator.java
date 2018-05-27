afa1db0b1acc327b15f83414c3e57ebb76287cb0
Ruben van Loen <rubenski@gmail.com>
2018-05-27 18:47:22 +0200

    Comments

--- src/main/java/nl/codebase/entities/iam/exception/IAMResponseExceptionTranslator.java
+++ src/main/java/nl/codebase/entities/iam/exception/IAMResponseExceptionTranslator.java
@@ -8,7 +8,8 @@ import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
 
 /**
- * Custom translator for OAuth exceptions
+ * Custom translator for OAuth exceptions. This translator sends more info to the frontend, so that the frontend
+ * can distinguish between disabled accounts and bad credentials.
  */
 @Slf4j
 public class IAMResponseExceptionTranslator implements WebResponseExceptionTranslator {
@@ -18,7 +19,8 @@ public class IAMResponseExceptionTranslator implements WebResponseExceptionTrans
         log.info("IAMResponseExceptionTranslator handler called");
         if (e instanceof InvalidGrantException) {
             InvalidGrantException invalidGrantException = (InvalidGrantException) e;
-            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON)
+            return ResponseEntity.status(400)
+                    .contentType(MediaType.APPLICATION_JSON)
                     .body(new IAMOAuthException(invalidGrantException.getMessage()));
         } else {
             throw e;
